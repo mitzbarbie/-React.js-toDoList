619b47b9491d78516c3e3b02553a74f78d435b3e
SeungHwan Hong <mitzbarbie@naver.com>
2022-05-09 14:51:53 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+// useEffect is a function that takes two arguments
 
 function App() {
+  const [counter, setValue] = useState(0);
+  // [value, modifier function]
+  // value : value / setValue : value를 수정하기 위한 function
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+
+  // const iRunOnlyOnce = () => {
+  //   console.log("i run only once");
+  // };
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  // useEffect function allows us the basically protect our code to only be called one time. This is why we use useEffect function.
+
   return (
     <div>
-      <h1 className={styles.title}>hello!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
