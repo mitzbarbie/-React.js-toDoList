53d0a209b701f0411723e872c15182253620a590
SeungHwan Hong <mitzbarbie@naver.com>
2022-05-09 16:34:08 +0900

    to-do list part one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  function byFn() {
-    console.log("bb");
-  }
-  function hiFn() {
-    console.log("hh");
-    return byFn;
-  }
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+
+  console.log(toDos);
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        ></input>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
