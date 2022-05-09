c0fb42e4f5ea893a45e236c180bf5e6dae2021ab
SeungHwan Hong <mitzbarbie@naver.com>
2022-05-09 15:42:06 +0900

    cleanup useing by useEffect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
 import { useState, useEffect } from "react";
-// useEffect is a function that takes two arguments
+
+function Hello() {
+  function byFn() {
+    console.log("bb");
+  }
+  function hiFn() {
+    console.log("hh");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  // [value, modifier function]
-  // value : value / setValue : value를 수정하기 위한 function
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-  console.log("i run all the time");
-
-  // const iRunOnlyOnce = () => {
-  //   console.log("i run only once");
-  // };
-  // useEffect(iRunOnlyOnce, []);
-  useEffect(() => {
-    console.log("CALL THE API...");
-  }, []);
-
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
-  // useEffect function allows us the basically protect our code to only be called one time. This is why we use useEffect function.
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here!"
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
